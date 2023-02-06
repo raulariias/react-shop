8c40c118ad1decce63e345fb51574f2b5defd658
raulariias <raularias24685@gmail.com>
2023-02-05 22:11:23 -0400

    First Commit

---
+++ src/containers/ProductDetail.jsx
@@ -0,0 +1,17 @@
+import ProductInfo from '../components/ProductInfo';
+import '../styles/ProductDetail.scss';
+
+import pdIconClose from "@icons/icon_close.png"
+
+const ProductDetail = () => {
+	return (
+		<aside className="ProductDetail">
+			<div className="ProductDetail-close">
+				<img src={pdIconClose} alt="close" />
+			</div>
+			<ProductInfo />
+		</aside>
+	);
+}
+
+export default ProductDetail;
