b04235ea68621972f6b797f54a7092111a45a45d
raulariias <raularias24685@gmail.com>
2023-02-06 16:35:16 -0400

    Bug Hunters #1 resuelto

---
+++ webpack.config.dev.js
@@ -0,0 +1,89 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "bundle.js",
+    },
+    mode: 'development',
+    resolve: {
+        extensions: ['.js', '.jsx'],
+        alias: {
+            '@components': path.resolve(__dirname, 'src/components/'),
+            '@containers': path.resolve(__dirname, 'src/containers/'),
+            '@pages': path.resolve(__dirname, 'src/pages/'),
+            '@styles': path.resolve(__dirname, 'src/styles/'),
+            '@hooks': path.resolve(__dirname, 'src/hooks/'),
+            '@context': path.resolve(__dirname, 'src/context/'),
+            '@icons': path.resolve(__dirname, 'src/assets/icons/'),
+            '@logos': path.resolve(__dirname, 'src/assets/logos/'),
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env',
+                            [
+                                '@babel/preset-react',
+                                {
+                                    runtime: 'automatic',
+                                }
+                            ],
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader",
+                ]
+            },
+            {
+                test: /\.(png|jpg|svg|jpeg|webp)$/,
+	    /*aquí en test agregas la expresión regular para procesar los diferentes tipos de imagenes que tengas.*/
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/pictures/[hash][ext]',
+		/*aquí en filename pones la carpeta en donde quieres que se guarden tus imagenes (le agrego el [hash] para evitar problemas con el cache, además [ext] hace referencia a la extensión del archivo que se haya procesado).*/
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        }),
+    ],
+    devServer: {
+        historyApiFallback: true,
+        static: {
+          directory: path.join(__dirname, 'public'),
+          },
+        compress: true,
+        port: 3005,
+      }
+}
