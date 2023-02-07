bba3aa89a3bdcd8ff39d694ce9b118760799977d
raulariias <raularias24685@gmail.com>
2023-02-06 22:26:08 -0400

    Bug Hunters #4 Resuelto

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -2,14 +2,14 @@ import { useContext } from 'react';
 import '@styles/ProductItem.scss';
 import AppContext from '@context/AppContext'
 import btAddCart from "@icons/bt_add_to_cart.svg";
+import btAddedCart from "@icons/bt_added_to_cart.svg"
 
 const ProductItem = ({ product }) => {
-	const { addToCart } = useContext(AppContext);
-
-	const handleClick = item => {
-		addToCart(item);
-		console.log(item);
-	}
+	const { addToCart, removeFromCart, state } = useContext(AppContext);
+	const itsProductAdded = () => state.cart.some( (item) => item.id === product.id) ? true : false;
+	const handleClick = (item) => {
+		itsProductAdded() ? removeFromCart(item) : addToCart(item);
+	  };
 
 
 	return (
@@ -20,8 +20,12 @@ const ProductItem = ({ product }) => {
 					<p>${product.price}</p>
 					<p>{product.title}</p>
 				</div>
-				<figure onClick={() => handleClick(product)} >
-					<img src={btAddCart} alt="" />
+				<figure onClick={() => handleClick(product)}>
+					{ itsProductAdded() ? (
+						<img src={btAddedCart} alt="" className="btAddedCart"/>
+					) : (
+						<img src={btAddCart} alt="" className="btAddCart"/>
+					)}
 				</figure>
 			</div>
 		</div>
